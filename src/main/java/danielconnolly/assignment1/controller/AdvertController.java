67ff67f1323d03220abd28006c669ad5f0143ce4
Daniel Connolly <u1457227@unimail.hud.ac.uk>
2018-12-18 20:20:28 +0000

    Initial Commit

---
+++ src/main/java/danielconnolly/assignment1/controller/AdvertController.java
@@ -0,0 +1,18 @@
+package danielconnolly.assignment1.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AdvertController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index()
+    {
+        return "index";
+    }
+}
